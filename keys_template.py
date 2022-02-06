8ee9da4687d4a59cd21420f3847921b5ae414c7a
ip <ianperry99@gmail.com>
2022-02-06 02:23:03 -0500

    update

---
+++ keys_template.py
@@ -0,0 +1,16 @@
+# Binance Info
+api_key = ''
+api_secret = ''
+
+# CoinMarketBase Info
+cmb_apikey = ''
+
+# PostgreSql Info
+hostname = ''
+database = ''
+username = ''
+password = ''
+port = 5432
+
+# Directory where investomatic resides
+localdir = ''
